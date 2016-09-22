70581aeb91d82cd4e0d4c6d8ff68a9aae55016cc
Harshavardhana <harsha@minio.io>
2016-09-22 15:15:19 -0700

    Integrate bucket policies properly.

--- app/js/components/Policy.js
+++ app/js/components/Policy.js
@@ -1,7 +1,9 @@
 import { READ_ONLY, WRITE_ONLY, READ_WRITE } from '../actions'
 
 import React, { Component, PropTypes } from 'react'
+import connect from 'react-redux/lib/components/connect'
 import classnames from 'classnames'
+import * as actions from '../actions'
 
 class Policy extends Component {
   constructor(props, context) {
@@ -13,17 +15,22 @@ class Policy extends Component {
     this.setState({ policy: e.target.value })
   }
 
+  removePolicy(e) {
+    e.preventDefault()
+    const { dispatch } = this.props
+
+    dispatch(actions.removePolicy(policy.bucket, policy.prefix))
+  }
+
   render() {
-    const { policy, actions } = this.props
+    const { policy } = this.props
 
-    let element = (
-      <div className="pmb-list">
-
+    return (
+      <li className={classnames()}>
           <div className="pmbl-item">{policy.prefix}</div>
-
           <div className="pmbl-item">
-            <select className="form-control" 
-                    value={policy.policy} 
+            <select className="form-control"
+                    value={policy.policy}
                     onChange={this.handlePolicyChange.bind(this)}>
               <option value={READ_ONLY}>Read Only</option>
               <option value={WRITE_ONLY}>Write Only</option>
@@ -32,24 +39,14 @@ class Policy extends Component {
           </div>
 
           <div className="pmbl-item">
-            <button className="btn btn-sm btn-block btn-danger" onClick={() => actions.removePolicy(policy.bucket, policy.prefix)}>Remove</button>
+            <button className="btn btn-sm btn-block btn-danger" onClick={this.removePolicy.bind(this)}>Remove</button>
           </div>
-
-      </div>
-    )
-
-    return (
-      <li className={classnames()}>
-       {element}
       </li>
     )
   }
 }
 
 
-Policy.propTypes = {
-  policy: PropTypes.object.isRequired,
-  actions: PropTypes.object.isRequired
-}
-
-export default Policy
+export default connect(state => {
+    return { policy: state.policy }
+})(Policy)
--- app/js/components/PolicyInput.js
+++ app/js/components/PolicyInput.js
@@ -1,48 +1,56 @@
 import { READ_ONLY, WRITE_ONLY, READ_WRITE } from '../actions'
 import React, { Component, PropTypes } from 'react'
+import connect from 'react-redux/lib/components/connect'
 import classnames from 'classnames'
+import * as actions from '../actions'
 
 class PolicyInput extends Component {
   constructor(props, context) {
     super(props, context)
       this.state = {
-	  bucket: this.props.bucket || '',
-	  prefix: '',
-	  policy: WRITE_ONLY
+          bucket: this.props.bucket || '',
+          prefix: this.props.prefix || '',
+          policy: READ_ONLY
       }
   }
 
   handleBucketPrefixChange(e) {
-    this.setState({ prefix: e.target.value.trim() })
+      this.setState({ prefix: e.target.value.trim() })
   }
 
   handlePolicyChange(e) {
     this.setState({ policy: e.target.value })
   }
 
-  handlePolicySubmit() {
-    if (this.state.prefix.length > 0) {
-      alert("bucket=" + this.state.bucket + " prefix=" + this.state.prefix + " policy=" + this.state.policy)
-      this.props.addPolicy(this.state.bucket, this.state.prefix, this.state.policy)
-    }
-
-    this.setState({ bucket: this.props.bucket, prefix: '', policy: READ_WRITE })
+  handlePolicySubmit(e) {
+    e.preventDefault()
+    const { web, dispatch } = this.props
+    dispatch(actions.hideBucketPolicy())
+    web.SetBucketPolicy({
+        bucketName: this.state.bucket,
+        prefix: this.state.prefix,
+        policy: this.state.policy
+    })
+    .then(() => dispatch(actions.addPolicy(this.state.bucket, this.state.prefix, this.state.policy)))
+    .catch(e => dispatch(actions.showAlert({
+        type: 'danger',
+        message: e.message,
+    })))
   }
 
   render() {
     return (
       <header className="pmb-list">
-
         <div className="pmbl-item">
-          <input type="text" 
-                 className="form-control" 
-                 editable={true} 
-                 defaultValue={this.state.bucket} 
+          <input type="text"
+                 className="form-control"
+                 editable={true}
+                 defaultValue={this.state.prefix}
                  onChange={this.handleBucketPrefixChange.bind(this)} />
         </div>
 
         <div className="pmbl-item">
-          <select value={this.state.policy} 
+          <select value={this.state.policy}
                   onChange={this.handlePolicyChange.bind(this)}>
             <option value={READ_ONLY}>Read Only</option>
             <option value={WRITE_ONLY}>Write Only</option>
@@ -51,7 +59,7 @@ class PolicyInput extends Component {
         </div>
 
         <div className="pmbl-item">
-          <button className="btn btn-sm btn-block btn-primary" onClick={() => this.handlePolicySubmit()}>Add</button>
+          <button className="btn btn-sm btn-block btn-primary" onClick={this.handlePolicySubmit.bind(this)}>Add</button>
         </div>
 
       </header>
@@ -61,7 +69,8 @@ class PolicyInput extends Component {
 
 PolicyInput.propTypes = {
   bucket: PropTypes.string,
-  addPolicy: PropTypes.func.isRequired
 }
 
-export default PolicyInput
+export default connect(state => {
+    return { web: state.web }
+})(PolicyInput)
